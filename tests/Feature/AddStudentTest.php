2e9281d81b9001f2d9860b54632352a28f354eb5
Harry Preston <hpreston2909@gmail.com>
2021-04-12 15:45:56 +0100

    Written first test to assert that we get an OK response and that the count in the DB for students is 1

--- tests/Feature/AddStudentTest.php
+++ tests/Feature/AddStudentTest.php
@@ -8,7 +8,17 @@ use Tests\TestCase;
 class AddStudentTest extends TestCase
 {
     /** @test */
-    public function addStudentToDB(){
+    public function addStudentToDB()
+    {
+        $response = $this->post('/students' , [
+            'name' => 'Example Student Name',
+            'id' => 'Example student ID',
+            'address' => 'Example student address',
+            'telephone' => 'Example student telephone',
+            'year' => 'Example student year'
+        ]);
 
+        $response->assertOk();
+        $this->assertCount(1, Student::all());
     }
 }

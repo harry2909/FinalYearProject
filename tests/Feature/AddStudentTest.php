4f32eec08df4b42c1905c304b1f2dc25576b5c2b
Harry Preston <hpreston2909@gmail.com>
2021-04-12 15:39:05 +0100

    Setting up first example test case

---
+++ tests/Feature/AddStudentTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class AddStudentTest extends TestCase
+{
+    /** @test */
+    public function addStudentToDB(){
+
+    }
+}
